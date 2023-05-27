020243404cc22ac1214919e6a003f8bc0703393f
xVicArT <aristizabal9200@gmail.com>
2023-05-27 00:44:27 -0500

    Add (Styles - JSX component folders)

---
+++ proyecto-react/src/Components/JSX/Footer.jsx
@@ -0,0 +1,43 @@
+import './Components/Styles/Footer.css'
+
+export const Footer = () => {
+  return (
+
+      <footer>
+        <nav className='socialmedia'>
+
+           <a href="#" target="_blank" rel="external"/><img src="#" alt="GitHub icon"
+          title="#"/>
+
+           <a href="#" target="_blank" rel="external"/><img src="#"
+          alt="Facebook icon top" title="#"/>
+
+           <a href="#" target="_blank" rel="external"/><img src="#"
+          alt="Instagram icon" title="#"/>
+
+           <a href="#" target="_blank" rel="external"/><img src="#"
+          alt="Twitter icon" title="#"/>
+
+           <a href="#" target="_blank" rel="external"/><img src="#"
+          alt="Youtube icon" title="#"/>
+
+        </nav>
+        <ul className='footer-lista-horizontal'>
+          <li><img src="#" alt="1" /></li>
+          <li><img src="#" alt="#" /></li>
+          <li><img src="#" alt="#" /></li>
+          <li><img src="#" alt="#" /></li>
+          <li><img src="#" alt="#" /></li>
+          <li><img src="#" alt="#" /></li>
+          <li><img src="#" alt="#" /></li>
+          <li><img src="#" alt="#" /></li>
+          <li><img src="#" alt="#" /></li>
+          <li><img src="#" alt="#" /></li>
+          <li><img src="#" alt="#" /></li>
+          <li><img src="#" alt="12" /></li>
+        </ul>
+
+  </footer>
+
+  )
+}
