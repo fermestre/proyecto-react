a5bb941a8d34ee713fff2193af269dbf9cf95422
xVicArT <aristizabal9200@gmail.com>
2023-06-02 16:13:00 -0500

    Filtros (Primera update)

--- proyecto-react/src/Components/JSX/Bodygrid.jsx
+++ proyecto-react/src/Components/JSX/Bodygrid.jsx
@@ -1,69 +1,125 @@
-import React from 'react'
+import React, { useState } from 'react'
 import imagenes from "../Images/Imagenes.jsx";
 import "../Styles/Bodygrid.css"
 
 
 
+
 export const Bodygrid = () => {
+
+  const [showUkelele, setShowUkelele] = useState(false);
+  const [showGuitarraA, setShowGuitarraA] = useState(false);
+  const [showGuitarraE, setShowGuitarraE] = useState(false);
+
+  const handleUkeleleToggle = () => {
+    setShowUkelele(!showUkelele);
+  };
+
+  const handleGuitarraAToggle = () => {
+    setShowGuitarraA(!showGuitarraA);
+  };
+
+  const handleGuitarraEToggle = () => {
+    setShowGuitarraE(!showGuitarraE);
+  };
+
+  
+
   return (
 
     <React.Fragment>
+
       <div className="bodygrid-globalcontainer">
       <h1>La Cuerda Mágica</h1>
     <section className="product-container">
     <aside className="filters">
       <article className="filter-box">
         <h3>Filtros:</h3>
-        <ul id='list1'>
-          <li><input type="checkbox" name="#" id="#"/> Ukelele</li>
-          <li><input type="checkbox" name="#" id="#"/> Guitarra acustica</li>
-          <li><input type="checkbox" name="#" id="#"/> Guitarra electrica</li>
+        <ul id="list1">
+        <div>
+          <input 
+         type="checkbox"
+         id="ukelele"
+         value="ukelele"
+         checked={showUkelele}
+         onChange={handleUkeleleToggle}
 
-        </ul>
-        <ul id='list2'>
-          <li><input type="checkbox" name="#" id="#"/> Rojo</li>
-          <li><input type="checkbox" name="#" id="#"/> Azul</li>
-          <li><input type="checkbox" name="#" id="#"/> Negro</li>
-          <li><input type="checkbox" name="#" id="#"/> Blanco</li>
-          <li><input type="checkbox" name="#" id="#"/> Madera</li>
+          />
+            <label htmlFor="ukelele"> Ukelele</label>
+        </div>
+          <div>
+            <input 
+          type="checkbox" 
+          name="instrumentos" 
+          value="fguitarraacustica"
+          id="fguitarraacustica"
+          checked={showGuitarraA}
+         onChange={handleGuitarraAToggle}
+
+          />
+            <label htmlFor="guitarra-acustica"> Guitarra Acustica</label> 
+            </div>
+
+          <div>    
+
+             <input 
+          type="checkbox" 
+          name="instrumentos" 
+          value="fguitarraelectrica" 
+          id="fguitarraelectrica"
+          checked={showGuitarraE}
+          onChange={handleGuitarraEToggle}/>
+            <label htmlFor="Ukelele"> Guitarra Electrica</label> 
+            </div>
 
         </ul>
       </article>
     </aside>
 
+
+
+
     <article className="product-grid">
-      <figure className="product-item">
+        
+    {showGuitarraA && (
+      <figure className="product-item" name="guitarraacustica" >
         <p>5.00 ⭐</p>
-        <img src={imagenes.producto1} alt="Product 1"/>
+        <img src={imagenes.producto1} alt="Product 4"/>
         <figcaption>Fender CD-60S</figcaption>
         <p>$1.200.000</p>
         <button>Añadir al carrito</button>
       </figure>
+    )}
 
-      <figure className="product-item">
+{showGuitarraA && (
+      <figure className="product-item" name="guitarraacustica">
         <p>5.00 ⭐</p>
-        <img src={imagenes.producto2} alt="Product 1"/>
+        <img src={imagenes.producto2} alt="Product 4"/>
         <figcaption>Fender Tim Armstrong Hell</figcaption>
         <p>$2.500.000</p>
         <button>Añadir al carrito</button>
       </figure>
-
-      <figure className="product-item">
+)}
+ {showGuitarraA && (
+      <figure className="product-item" name="guitarraacustica">
         <p>5.00 ⭐</p>
-        <img src={imagenes.producto3} alt="Product 1"/>
+        <img src={imagenes.producto3} alt="Product 4"/>
         <figcaption>Fender LE PO-220E Orch.</figcaption>
         <p>$3.965.000</p>
         <button>Añadir al carrito</button>
       </figure>
-
-      <figure className="product-item">
+ )}
+      {showGuitarraA && (
+      <figure className="product-item" name="guitarraacustica">
         <p>5.00 ⭐</p>
-        <img src={imagenes.producto4} alt="Product 1"/>
+        <img src={imagenes.producto4} alt="Product 4"/>
         <figcaption>Fender FA-135CE Concert</figcaption>
         <p>$1.200.000</p>
         <button>Añadir al carrito</button>
       </figure>
+      )}
 
+{showGuitarraE && (
       <figure className="product-item">
         <p>5.00 ⭐</p>
         <img src={imagenes.producto5} alt="Product 1"/>
@@ -71,8 +127,8 @@ export const Bodygrid = () => {
         <p>$4.220.000</p>
         <button>Añadir al carrito</button>
       </figure>
-
-      
+)}
+     {showGuitarraE && (
       <figure className="product-item">
         <p>5.00 ⭐</p>
         <img src={imagenes.producto6} alt="Product 1"/>
@@ -80,8 +136,9 @@ export const Bodygrid = () => {
         <p>$5.600.000</p>
         <button>Añadir al carrito</button>
       </figure>
-
+     )}
       
+      {showGuitarraE && (
       <figure className="product-item">
         <p>5.00 ⭐</p>
         <img src={imagenes.producto7} alt="Product 1"/>
@@ -89,8 +146,9 @@ export const Bodygrid = () => {
         <p>$14.350.000</p>
         <button>Añadir al carrito</button>
       </figure>
-
-      
+      )}
+ 
+ {showGuitarraE && (
       <figure className="product-item">
         <p>5.00 ⭐</p>
         <img src={imagenes.producto8} alt="Product 1"/>
@@ -98,44 +156,54 @@ export const Bodygrid = () => {
         <p>$7.050.000</p>
         <button>Añadir al carrito</button>
       </figure>
+ )}
 
-      
+      {showUkelele && (
+    
       <figure className="product-item">
         <p>5.00 ⭐</p>
-        <img src={imagenes.producto9} alt="Product 1"/>
+        <img src={imagenes.producto9} alt="Product 9" name="ukelele"/>
         <figcaption>Lanikai MA-C Concierto</figcaption>
         <p>$150.000</p>
         <button>Añadir al carrito</button>
       </figure>
 
-      
+      )}
+
+{showUkelele && (
+
       <figure className="product-item">
         <p>5.00 ⭐</p>
-        <img src={imagenes.producto10} alt="Product 1"/>
+        <img src={imagenes.producto10} alt="Product 10" name="ukelele"/>
         <figcaption>3x Kahakai SOP CON</figcaption>
         <p>$400.000</p>
         <button>Añadir al carrito</button>
       </figure>
 
-      
+)}
+{showUkelele && (
+    
       <figure className="product-item">
         <p>5.00 ⭐</p>
-        <img src={imagenes.producto11} alt="Product 1"/>
-        <figcaption>KA-S Ukelele Kala Soprano</figcaption>
+        <img src={imagenes.producto11} alt="Product 11" name="ukelele"/>
+        <figcaption>KA-S Ukelele Soprano</figcaption>
         <p>$450.000</p>
         <button>Añadir al carrito</button>
       </figure>
-
       
+)}
+{showUkelele && (
+    
       <figure className="product-item">
         <p>5.00 ⭐</p>
-        <img src={imagenes.producto12} alt="Product 1"/>
+        <img src={imagenes.producto12} alt="Product 12" name="ukelele"/>
         <figcaption>BS1 Soprano</figcaption>
         <p>$300.000</p>
         <button>Añadir al carrito</button>
       </figure>
 
-      
+)}
+ {showGuitarraA && (
       <figure className="product-item">
         <p>5.00 ⭐</p>
         <img src={imagenes.producto13} alt="Product 1"/>
@@ -143,8 +211,8 @@ export const Bodygrid = () => {
         <p>$300.000</p>
         <button>Añadir al carrito</button>
       </figure>
-
-      
+ )}
+       {showGuitarraA && (
       <figure className="product-item">
         <p>5.00 ⭐</p>
         <img src={imagenes.producto14} alt="Product 1"/>
@@ -152,8 +220,8 @@ export const Bodygrid = () => {
         <p>$600.000</p>
         <button>Añadir al carrito</button>
       </figure>
-
-      
+       )}
+      {showGuitarraA && (
       <figure className="product-item">
         <p>5.00 ⭐</p>
         <img src={imagenes.producto15} alt="Product 1"/>
@@ -161,8 +229,9 @@ export const Bodygrid = () => {
         <p>$1.150.000</p>
         <button>Añadir al carrito</button>
       </figure>
+      )}
 
-      
+      {showGuitarraA && (
       <figure className="product-item">
         <p>5.00 ⭐</p>
         <img src={imagenes.producto16} alt="Product 1"/>
@@ -170,8 +239,8 @@ export const Bodygrid = () => {
         <p>$500.000</p>
         <button>Añadir al carrito</button>
       </figure>
-
-      
+      )}
+      {showGuitarraE && (
       <figure className="product-item">
         <p>5.00 ⭐</p>
         <img src={imagenes.producto17} alt="Product 1"/>
@@ -179,8 +248,9 @@ export const Bodygrid = () => {
         <p>$1.600.000</p>
         <button>Yamaha Pacific 112J</button>
       </figure>
-
+      )}
       
+      {showGuitarraE && (
       <figure className="product-item">
         <p>5.00 ⭐</p>
         <img src={imagenes.producto18} alt="Product 1"/>
@@ -188,8 +258,9 @@ export const Bodygrid = () => {
         <p>$1.000.000</p>
         <button>Añadir al carrito</button>
       </figure>
-
+      )}
       
+      {showGuitarraE && (
       <figure className="product-item">
         <p>5.00 ⭐</p>
         <img src={imagenes.producto19} alt="Product 1"/>
@@ -197,8 +268,9 @@ export const Bodygrid = () => {
         <p>$1.400.000</p>
         <button>Añadir al carrito</button>
       </figure>
-
+      )}
       
+      {showGuitarraE && (
       <figure className="product-item">
         <p>5.00 ⭐</p>
         <img src={imagenes.producto20} alt="Product 1"/>
@@ -206,11 +278,7 @@ export const Bodygrid = () => {
         <p>$1.350.000</p>
         <button>Añadir al carrito</button>
       </figure>
-
-
-
-
-
+      )}
 
     </article>
 
@@ -231,5 +299,5 @@ export const Bodygrid = () => {
 
 </React.Fragment>
 
-  )
-}
+  );
+};
