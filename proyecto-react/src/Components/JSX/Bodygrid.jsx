3ae85b68b3b1d927514a06a0f897c759602f251c
xVicArT <aristizabal9200@gmail.com>
2023-06-02 10:33:55 -0500

    UpdateF2(Imagenes,precios,stickyfiltersbox,name)

--- proyecto-react/src/Components/JSX/Bodygrid.jsx
+++ proyecto-react/src/Components/JSX/Bodygrid.jsx
@@ -15,20 +15,17 @@ export const Bodygrid = () => {
       <article className="filter-box">
         <h3>Filtros:</h3>
         <ul id='list1'>
-          <li><input type="checkbox" name="#" id="#"/> Bateria</li>
-          <li><input type="checkbox" name="#" id="#"/> Viola y Violin</li>
-          <li><input type="checkbox" name="#" id="#"/> Piano y teclado</li>
+          <li><input type="checkbox" name="#" id="#"/> Ukelele</li>
           <li><input type="checkbox" name="#" id="#"/> Guitarra acustica</li>
           <li><input type="checkbox" name="#" id="#"/> Guitarra electrica</li>
 
         </ul>
         <ul id='list2'>
-        <li><input type="checkbox" name="#" id="#"/> Rojo</li>
-          <li><input type="checkbox" name="#" id="#"/> Rosa</li>
-          
+          <li><input type="checkbox" name="#" id="#"/> Rojo</li>
+          <li><input type="checkbox" name="#" id="#"/> Azul</li>
           <li><input type="checkbox" name="#" id="#"/> Negro</li>
           <li><input type="checkbox" name="#" id="#"/> Blanco</li>
-          <li><input type="checkbox" name="#" id="#"/> Morado</li>
+          <li><input type="checkbox" name="#" id="#"/> Madera</li>
 
         </ul>
       </article>
@@ -38,34 +35,178 @@ export const Bodygrid = () => {
       <figure className="product-item">
         <p>5.00 ⭐</p>
         <img src={imagenes.producto1} alt="Product 1"/>
-        <figcaption>Producto 1</figcaption>
-        <p>$500.000</p>
+        <figcaption>Fender CD-60S</figcaption>
+        <p>$1.200.000</p>
         <button>Añadir al carrito</button>
       </figure>
 
       <figure className="product-item">
         <p>5.00 ⭐</p>
-        <img src={imagenes.producto1} alt="Product 1"/>
-        <figcaption>Producto 2</figcaption>
-        <p>$500.000</p>
+        <img src={imagenes.producto2} alt="Product 1"/>
+        <figcaption>Fender Tim Armstrong Hell</figcaption>
+        <p>$2.500.000</p>
         <button>Añadir al carrito</button>
       </figure>
 
       <figure className="product-item">
         <p>5.00 ⭐</p>
-        <img src={imagenes.producto1} alt="Product 1"/>
-        <figcaption>Producto 3</figcaption>
-        <p>$500.000</p>
+        <img src={imagenes.producto3} alt="Product 1"/>
+        <figcaption>Fender LE PO-220E Orch.</figcaption>
+        <p>$3.965.000</p>
         <button>Añadir al carrito</button>
       </figure>
 
       <figure className="product-item">
         <p>5.00 ⭐</p>
-        <img src={imagenes.producto1} alt="Product 1"/>
-        <figcaption>Producto 4</figcaption>
+        <img src={imagenes.producto4} alt="Product 1"/>
+        <figcaption>Fender FA-135CE Concert</figcaption>
+        <p>$1.200.000</p>
+        <button>Añadir al carrito</button>
+      </figure>
+
+      <figure className="product-item">
+        <p>5.00 ⭐</p>
+        <img src={imagenes.producto5} alt="Product 1"/>
+        <figcaption>Player Stratocaster® HSS</figcaption>
+        <p>$4.220.000</p>
+        <button>Añadir al carrito</button>
+      </figure>
+
+      
+      <figure className="product-item">
+        <p>5.00 ⭐</p>
+        <img src={imagenes.producto6} alt="Product 1"/>
+        <figcaption>Player + Stratocaster® HSS</figcaption>
+        <p>$5.600.000</p>
+        <button>Añadir al carrito</button>
+      </figure>
+
+      
+      <figure className="product-item">
+        <p>5.00 ⭐</p>
+        <img src={imagenes.producto7} alt="Product 1"/>
+        <figcaption>Landau Stratocaster®</figcaption>
+        <p>$14.350.000</p>
+        <button>Añadir al carrito</button>
+      </figure>
+
+      
+      <figure className="product-item">
+        <p>5.00 ⭐</p>
+        <img src={imagenes.producto8} alt="Product 1"/>
+        <figcaption>Tash Sultana Stratocaster®</figcaption>
+        <p>$7.050.000</p>
+        <button>Añadir al carrito</button>
+      </figure>
+
+      
+      <figure className="product-item">
+        <p>5.00 ⭐</p>
+        <img src={imagenes.producto9} alt="Product 1"/>
+        <figcaption>Lanikai MA-C Concierto</figcaption>
+        <p>$150.000</p>
+        <button>Añadir al carrito</button>
+      </figure>
+
+      
+      <figure className="product-item">
+        <p>5.00 ⭐</p>
+        <img src={imagenes.producto10} alt="Product 1"/>
+        <figcaption>3x Kahakai SOP CON</figcaption>
+        <p>$400.000</p>
+        <button>Añadir al carrito</button>
+      </figure>
+
+      
+      <figure className="product-item">
+        <p>5.00 ⭐</p>
+        <img src={imagenes.producto11} alt="Product 1"/>
+        <figcaption>KA-S Ukelele Kala Soprano</figcaption>
+        <p>$450.000</p>
+        <button>Añadir al carrito</button>
+      </figure>
+
+      
+      <figure className="product-item">
+        <p>5.00 ⭐</p>
+        <img src={imagenes.producto12} alt="Product 1"/>
+        <figcaption>BS1 Soprano</figcaption>
+        <p>$300.000</p>
+        <button>Añadir al carrito</button>
+      </figure>
+
+      
+      <figure className="product-item">
+        <p>5.00 ⭐</p>
+        <img src={imagenes.producto13} alt="Product 1"/>
+        <figcaption>Yamaha STORIA I</figcaption>
+        <p>$300.000</p>
+        <button>Añadir al carrito</button>
+      </figure>
+
+      
+      <figure className="product-item">
+        <p>5.00 ⭐</p>
+        <img src={imagenes.producto14} alt="Product 1"/>
+        <figcaption>Yamaha C40</figcaption>
+        <p>$600.000</p>
+        <button>Añadir al carrito</button>
+      </figure>
+
+      
+      <figure className="product-item">
+        <p>5.00 ⭐</p>
+        <img src={imagenes.producto15} alt="Product 1"/>
+        <figcaption>Yamaha F370</figcaption>
+        <p>$1.150.000</p>
+        <button>Añadir al carrito</button>
+      </figure>
+
+      
+      <figure className="product-item">
+        <p>5.00 ⭐</p>
+        <img src={imagenes.producto16} alt="Product 1"/>
+        <figcaption>Yamaha Classic C-70</figcaption>
         <p>$500.000</p>
         <button>Añadir al carrito</button>
       </figure>
+
+      
+      <figure className="product-item">
+        <p>5.00 ⭐</p>
+        <img src={imagenes.producto17} alt="Product 1"/>
+        <figcaption>Yamaha Pacific 112VM</figcaption>
+        <p>$1.600.000</p>
+        <button>Yamaha Pacific 112J</button>
+      </figure>
+
+      
+      <figure className="product-item">
+        <p>5.00 ⭐</p>
+        <img src={imagenes.producto18} alt="Product 1"/>
+        <figcaption>Yamaha Pacific 012</figcaption>
+        <p>$1.000.000</p>
+        <button>Añadir al carrito</button>
+      </figure>
+
+      
+      <figure className="product-item">
+        <p>5.00 ⭐</p>
+        <img src={imagenes.producto19} alt="Product 1"/>
+        <figcaption>Yamaha Pacific 120H</figcaption>
+        <p>$1.400.000</p>
+        <button>Añadir al carrito</button>
+      </figure>
+
+      
+      <figure className="product-item">
+        <p>5.00 ⭐</p>
+        <img src={imagenes.producto20} alt="Product 1"/>
+        <figcaption>Yamaha Pacific 112J</figcaption>
+        <p>$1.350.000</p>
+        <button>Añadir al carrito</button>
+      </figure>
+
 
 
 
